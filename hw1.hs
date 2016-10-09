c6537dd9c07c13d6e3828a4dcac596d54d7ec549
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2016-10-09 16:05:26 +0530

    removed commented code

--- hw1.hs
+++ hw1.hs
@@ -10,26 +10,12 @@ toDigits num
   | otherwise = toDigits (quot num 10) ++ [rem num 10]
 ----------------------------------------------------------------------------------
 
-nth :: [a] -> Int -> a
-nth list index
-  | length list < pred index = error "index out of bound"
-  | otherwise = head . drop(index) $ list
-----------------------------------------------------------------------------------
-
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev num = reverse . toDigits $ num
 ----------------------------------------------------------------------------------
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther list = map (\ pair -> fst pair $ snd pair) (zip (cycle [(*) 1, (*) 2]) list)
-
--- Pooja and Prasenjit's idiot concat thing
--- doubleEveryOther :: [Integer] -> [Integer]
--- doubleEveryOther list
---   | list == [] = []
---   | length list == 1 = list
---   | length list <= 2 = (head list) : [2 * (list !! 1)]
---   | otherwise = (doubleEveryOther (take 2 list)) ++ (doubleEveryOther (drop 2 list))
 ---------------------------------------------------------------------------------
 
 sumDigits :: [Integer] -> Integer
@@ -46,3 +32,4 @@ hanoi num peg1 peg2 peg3
   | otherwise = hanoi numberOfPegsLeft peg1 peg3 peg2 ++ [(peg1, peg2)] ++ hanoi numberOfPegsLeft peg3 peg2 peg1
   where
     numberOfPegsLeft = pred num
+---------------------------------------------------------------------------------
